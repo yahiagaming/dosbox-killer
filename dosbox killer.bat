016d26c0dafbb4590c7ba24c24b8e371e60c671a
yahiagaming <154459888+yahiagaming@users.noreply.github.com>
2024-08-25 13:32:01 +0300

    Add files via upload

---
+++ dosbox killer.bat
@@ -0,0 +1,9 @@
+@echo off
+cd C:\Users\yahia\AppData\Local\DOSBox
+del dosbox-0.74-3.conf
+cd C:\Program Files\DOSBox-0.74-3\Video Codec
+del zmbv.dll
+cd C:\Program Files\DOSBox-0.74-3
+del SDL.dll
+echo dosbox killed open it now and you well see
+pause;
